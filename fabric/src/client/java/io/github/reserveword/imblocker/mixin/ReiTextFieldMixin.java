93c3cc7d0edca5e1b835366cbf54a9897117404e
ghostflyby <ghostflyby+git@outlook.com>
2023-12-28 20:00:28 +0800

    Check Screen not null for rei

--- fabric/src/client/java/io/github/reserveword/imblocker/mixin/ReiTextFieldMixin.java
+++ fabric/src/client/java/io/github/reserveword/imblocker/mixin/ReiTextFieldMixin.java
@@ -1,6 +1,7 @@
 package io.github.reserveword.imblocker.mixin;
 
 import io.github.reserveword.imblocker.IMCheckState;
+import net.minecraft.client.MinecraftClient;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Pseudo;
 import org.spongepowered.asm.mixin.Shadow;
@@ -17,14 +18,16 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 public abstract class ReiTextFieldMixin {
     @Shadow
     protected boolean editable;
+
     @Shadow
     public abstract boolean isVisible();
+
     @Shadow
     public abstract boolean isFocused();
 
     @Inject(method = "tick", at = @At("HEAD"))
     public void tickCallback(CallbackInfo ci) {
-        boolean active = this.isVisible() && this.isFocused() && this.editable;
+        boolean active = this.isVisible() && this.isFocused() && this.editable && MinecraftClient.getInstance().currentScreen != null;
         IMCheckState.captureTick(this, active);
     }
 

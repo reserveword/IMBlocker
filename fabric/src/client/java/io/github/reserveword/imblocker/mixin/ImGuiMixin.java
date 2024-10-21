001e97b45a2ed4fc4af6f6b1a3f20fac8944cc7a
LitnhJacuzzi <2075397019@qq.com>
2024-10-21 18:25:59 +0800

    Support Axiom Mod.

---
+++ fabric/src/client/java/io/github/reserveword/imblocker/mixin/ImGuiMixin.java
@@ -0,0 +1,35 @@
+package io.github.reserveword.imblocker.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Pseudo;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import imgui.callback.ImGuiInputTextCallback;
+import imgui.internal.ImGui;
+import imgui.type.ImString;
+import io.github.reserveword.imblocker.FocusableWidgetAccessor;
+import io.github.reserveword.imblocker.IMCheckState;
+
+@Pseudo
+@Mixin(targets = "imgui.ImGui")
+public abstract class ImGuiMixin {
+	private static FocusableWidgetAccessor imguiAccessor =  () -> true;
+	private static boolean hasFocusedWidget = false;
+	
+	@Inject(method = "preInputText(ZLjava/lang/String;Ljava/lang/String;Limgui/type/ImString;"
+			+ "FFILimgui/callback/ImGuiInputTextCallback;)Z", at = @At("HEAD"))
+	private static void preInputText(boolean multiline, String label, String hint, ImString text, float width,
+			float height, int flags, ImGuiInputTextCallback callback, CallbackInfoReturnable<Boolean> ci) {
+		boolean wantTextInput = ImGui.getIO().getWantTextInput();
+		if(hasFocusedWidget != wantTextInput) {
+			if(wantTextInput) {
+				IMCheckState.focusGained(imguiAccessor);
+			}else {
+				IMCheckState.focusLost(imguiAccessor);
+			}
+			hasFocusedWidget = wantTextInput;
+		}
+	}
+}

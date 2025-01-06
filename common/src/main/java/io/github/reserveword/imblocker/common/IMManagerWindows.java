25ee57e8fda42ded0a04bb26a0ddc4a848d07b6a
reserveword <reserveword@outlook.com>
2025-01-06 22:53:26 +0800

    回滚Windows平台下控制输入法状态的逻辑，分离输入法开关和中英文切换的逻辑，使用 ImmAssociateContext 控制输入法开关

--- common/src/main/java/io/github/reserveword/imblocker/common/IMManagerWindows.java
+++ common/src/main/java/io/github/reserveword/imblocker/common/IMManagerWindows.java
@@ -9,6 +9,8 @@ final class IMManagerWindows implements IMManager.PlatformIMManager {
 
     private static final User32 u = User32.INSTANCE;
     private static boolean state = true;
+    public static long cooldown = System.currentTimeMillis();
+    private static boolean eng = false;
 
     static {
         Native.register("imm32");
@@ -16,13 +18,27 @@ final class IMManagerWindows implements IMManager.PlatformIMManager {
 
     private static native WinNT.HANDLE ImmGetContext(WinDef.HWND hwnd);
 
+    private static native WinNT.HANDLE ImmAssociateContext(WinDef.HWND hwnd, WinNT.HANDLE himc);
+
     private static native boolean ImmReleaseContext(WinDef.HWND hwnd, WinNT.HANDLE himc);
+
+    private static native WinNT.HANDLE ImmCreateContext();
+
+    private static native boolean ImmDestroyContext(WinNT.HANDLE himc);
 
     private static native boolean ImmSetConversionStatus(WinNT.HANDLE himc, int fdwConversion, int fdwSentence);
 
     @Override
     public void setEnglishState(boolean english) {
-        setState(!english);
+        if (english == eng) return;
+        if (System.currentTimeMillis() - cooldown < 50) return;
+        WinDef.HWND hwnd = u.GetActiveWindow();
+        WinNT.HANDLE himc = ImmGetContext(hwnd);
+        if (himc != null) {
+            ImmSetConversionStatus(himc, english ? 0 : 1, 0);
+        }
+        ImmReleaseContext(hwnd, himc);
+        eng = english;
     }
 
     @Override
@@ -37,11 +53,21 @@ final class IMManagerWindows implements IMManager.PlatformIMManager {
 
     @Override
     public void setState(boolean on) {
-        if (state == on) return;
-        WinDef.HWND hwnd = u.GetActiveWindow();
-        WinNT.HANDLE himc = ImmGetContext(hwnd);
-        ImmSetConversionStatus(himc, on ? 1 : 0, 0);
-        ImmReleaseContext(hwnd, himc);
-        state = on;
+        boolean state = ImmGetContext(u.GetActiveWindow()) != null;
+        if (state != on) {
+            cooldown = System.currentTimeMillis();
+            WinDef.HWND hwnd = u.GetActiveWindow();
+            if (on) {
+                WinNT.HANDLE himc = ImmGetContext(hwnd);
+                if (himc == null) {
+                    ImmAssociateContext(hwnd, ImmCreateContext());
+                }
+                ImmReleaseContext(hwnd, himc);
+            } else {
+                WinNT.HANDLE himc = ImmAssociateContext(hwnd, null);
+                ImmDestroyContext(himc);
+            }
+            IMManagerWindows.state = on;
+        }
     }
 }

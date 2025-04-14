cbf0349029a16d7c96598127370a28d344d99757
LitnhJacuzzi <2075397019@qq.com>
2025-04-14 22:49:12 +0800

    Forge/Neoforge: Support FTB.

---
+++ forge/src/main/java/io/github/reserveword/imblocker/mixin/forge/FtbTextFieldMixin.java
@@ -0,0 +1,23 @@
+package io.github.reserveword.imblocker.mixin.forge;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import dev.ftb.mods.ftblibrary.ui.TextBox;
+import io.github.reserveword.imblocker.common.gui.MinecraftFocusableWidget;
+
+@Mixin(value = TextBox.class, remap = false)
+public abstract class FtbTextFieldMixin implements MinecraftFocusableWidget {
+    
+    @Override
+    public boolean isWidgetEditable() {
+    	return true;
+    }
+    
+    @Inject(method = "setFocused", at = @At("TAIL"))
+    public void focusChanged(boolean isFocused, CallbackInfo ci) {
+    	onFocusChanged(isFocused);
+    }
+}

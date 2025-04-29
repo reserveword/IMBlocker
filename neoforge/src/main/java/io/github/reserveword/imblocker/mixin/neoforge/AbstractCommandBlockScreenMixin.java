9efffc0bd1f7517ffb6de69e1da4dd18e7b5d406
LitnhJacuzzi <2075397019@qq.com>
2025-04-29 18:57:14 +0800

    Initialize command block input field as English state preferred.

---
+++ neoforge/src/main/java/io/github/reserveword/imblocker/mixin/neoforge/AbstractCommandBlockScreenMixin.java
@@ -0,0 +1,28 @@
+package io.github.reserveword.imblocker.mixin.neoforge;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import io.github.reserveword.imblocker.common.ChatCommandInputType;
+import io.github.reserveword.imblocker.common.Config;
+import io.github.reserveword.imblocker.common.gui.MinecraftFocusableWidget;
+import net.minecraft.client.gui.components.EditBox;
+import net.minecraft.client.gui.screens.inventory.AbstractCommandBlockEditScreen;
+
+@Mixin(AbstractCommandBlockEditScreen.class)
+public class AbstractCommandBlockScreenMixin {
+	
+	@Shadow
+	protected EditBox commandEdit;
+
+	@Inject(method = "init", at = @At("TAIL"))
+	private void setCommandInputEnglishState(CallbackInfo ci) {
+		if(Config.INSTANCE.getChatCommandInputType() == ChatCommandInputType.DISABLE_IM) {
+			((MinecraftFocusableWidget) commandEdit).setPreferredEditState(false); 
+		}
+		((MinecraftFocusableWidget) commandEdit).setPreferredEnglishState(true); 
+	}
+}

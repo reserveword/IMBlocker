dfc34854188c6e7b227c886c80735f865729e62a
LitnhJacuzzi <2075397019@qq.com>
2026-04-26 12:22:39 +0800

    Compatible with Axiom 5.4.1

--- common/src/main/java/io/github/reserveword/imblocker/common/gui/UniversalIMECandidateOverlay.java
+++ common/src/main/java/io/github/reserveword/imblocker/common/gui/UniversalIMECandidateOverlay.java
@@ -1,7 +1,7 @@
 package io.github.reserveword.imblocker.common.gui;
 
-import imgui.ImDrawList;
-import imgui.ImGui;
+import imgui.moulberry92.ImDrawList;
+import imgui.moulberry92.ImGui;
 import io.github.reserveword.imblocker.common.accessor.MinecraftClientAccessor;
 
 public class UniversalIMECandidateOverlay {
--- common/src/main/java/io/github/reserveword/imblocker/mixin/compat/AxiomEditorUIMixin.java
+++ common/src/main/java/io/github/reserveword/imblocker/mixin/compat/AxiomEditorUIMixin.java
@@ -2,14 +2,12 @@ package io.github.reserveword.imblocker.mixin.compat;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Pseudo;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import imgui.ImDrawList;
-import imgui.ImGui;
-import imgui.ImGuiIO;
+import imgui.moulberry92.ImDrawList;
+import imgui.moulberry92.ImGui;
 import io.github.reserveword.imblocker.common.IMBlockerCore;
 import io.github.reserveword.imblocker.common.gui.AxiomGuiMonitor;
 import io.github.reserveword.imblocker.common.gui.FocusContainer;
@@ -21,21 +19,13 @@ import io.github.reserveword.imblocker.common.gui.UniversalIMEPreeditOverlay;
 @Pseudo
 @Mixin(targets = "com.moulberry.axiom.editor.EditorUI", remap = false)
 public abstract class AxiomEditorUIMixin {
-	
-	@Shadow
-	public static ImGuiIO imGuiIO;
-	
 	@Inject(method = "init", at = @At("TAIL"))
 	private static void loadImGui(CallbackInfo ci) {
-		Class<?> editorUIClass = null;
-		try {
-			editorUIClass = Class.forName("com.moulberry.axiom.editor.EditorUI");
-		} catch (Exception e) {}
-		AxiomGuiMonitor.createInstance(imGuiIO, editorUIClass);
+		AxiomGuiMonitor.createInstance();
 		IMBlockerCore.registerClientTickEvent(AxiomGuiMonitor.getInstance()::tick);
 	}
 	
-	@Inject(method = "drawOverlayInternal", at = @At(value = "INVOKE", target = "Limgui/ImGui;render()V"))
+	@Inject(method = "drawOverlayInternal", at = @At(value = "INVOKE", target = "Limgui/moulberry92/ImGui;render()V"))
 	private static void renderUniversalPreeditOverlay(CallbackInfo ci) {
 		if(FocusManager.getFocusedContainer() == FocusContainer.IMGUI) {
 			ImDrawList graphics = ImGui.getForegroundDrawList();

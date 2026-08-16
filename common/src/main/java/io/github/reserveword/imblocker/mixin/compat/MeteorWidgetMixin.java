7d67bceabd74b639e4c92dd244a2f7647fcb593f
LitnhJacuzzi <2075397019@qq.com>
2026-08-16 11:02:46 +0800

    Fix other mods with special scale.

--- common/src/main/java/io/github/reserveword/imblocker/mixin/compat/MeteorWidgetMixin.java
+++ common/src/main/java/io/github/reserveword/imblocker/mixin/compat/MeteorWidgetMixin.java
@@ -27,7 +27,7 @@ public abstract class MeteorWidgetMixin implements MinecraftFocusableWidget {
 	
 	@Override
 	public Rectangle getBoundsAbs() {
-		return new Rectangle((int) x, (int) y, (int) width, (int) height);
+		return new Rectangle(getGuiScale(), (int) x, (int) y, (int) width, (int) height);
 	}
 	
 	@Override
